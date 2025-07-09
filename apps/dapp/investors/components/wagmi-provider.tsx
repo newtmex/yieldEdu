d34c398f7703403c2d495dcebfb158f4179a83cd
Kamasah-Dickson <kamasahdickson19@gmail.com>
2025-07-09 14:33:22 +0000

    add wagmi provider

--- apps/dapp/investors/components/wagmi-provider.tsx
+++ apps/dapp/investors/components/wagmi-provider.tsx
@@ -32,7 +32,7 @@ const metadata = {
 	name: "YieldEdu",
 	description:
 		"A decentralized education and a fixed-yield protocol that earns users guaranteed yields on their EDU tokens.",
-	url: "https://edu-chain-hackathon.vercel.app/",
+	url: process.env.NEXT_PUBLIC_WEBSITE_URL!,
 	icons: ["https://avatars.githubusercontent.com/u/179229932"],
 };
 
