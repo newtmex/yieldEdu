4c5bda7891ecac26d7c9337e7e559d49faea6936
newtmex <brightomex@gmail.com>
2025-07-15 10:23:40 +0100

    Add staking task for ETH, WEDU, and dEDU with error handling for required amount

---
+++ solidity/scripts/stake.ts
@@ -0,0 +1,77 @@
+import { task } from "hardhat/config";
+import { ISToken__factory, Staking } from "../typechain-types";
+import { ISToken } from "../typechain-types/contracts/tokens";
+import { formatEther, parseEther } from "ethers";
+
+task("stake", "Stake ETH, WEDU, or dEDU into the Staking contract")
+    .addParam("staking", "Address of the Staking contract")
+    .addParam("tokenType", "Token type enum (0 = Learner, 1 = Scholar)")
+    .addOptionalParam(
+        "type",
+        "Type of asset to stake: edu | wedu | dedu",
+        "edu"
+    )
+    .addOptionalParam(
+        "amount",
+        "Amount of WEDU/dEDU to stake (in wei, required if not using native ETH)"
+    )
+    .setAction(async ({ staking, tokenType, type, amount }, hre) => {
+        amount = parseEther(amount);
+        const [signer] = await hre.ethers.getSigners();
+        const stakingContract = await hre.ethers.getContractAt(
+            "Staking",
+            staking,
+            signer
+        );
+
+        let tx;
+
+        switch (type.toLowerCase()) {
+            case "wedu":
+                if (!amount)
+                    throw new Error("Amount is required for WEDU staking");
+                console.log(`Staking ${formatEther(amount)} WEDU...`);
+
+                const weduAddress = await stakingContract.callStatic
+                    ._getStakingStorage()
+                    .then((s) => s[0]);
+                const WEDU = await hre.ethers.getContractAt(
+                    "IERC20",
+                    weduAddress,
+                    signer
+                );
+                await WEDU.approve(staking, amount);
+                tx = await stakingContract.stakeWEDU(tokenType, amount);
+                break;
+
+            case "dedu":
+                if (!amount)
+                    throw new Error("Amount is required for dEDU staking");
+                console.log(`Staking ${formatEther(amount)} dEDU...`);
+
+                const dEDU = await stakingContract.callStatic
+                    ._getStakingStorage()
+                    .then((s) => s[1]);
+                const dEDUToken = await hre.ethers.getContractAt(
+                    "IERC20",
+                    dEDU,
+                    signer
+                );
+                await dEDUToken.approve(staking, amount);
+                tx = await stakingContract.stakeDEDU(tokenType, amount);
+                break;
+
+            default:
+                console.log(
+                    `Staking ${formatEther(amount || "0")} ETH as ${
+                        tokenType == 0 ? "Learner" : "Scholar"
+                    }...`
+                );
+                tx = await stakingContract.stakeEDU(tokenType, {
+                    value: amount || parseEther("1"),
+                });
+        }
+
+        const receipt = await tx.wait();
+        console.log("Transaction mined:", receipt?.hash);
+    });
